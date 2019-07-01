07cd456c61c5925830a640049859c24583d71a17
agustinmontero <agustinmontero87@gmail.com>
2019-07-01 14:54:47 -0300

    *

--- bash/internet_return.sh
+++ bash/internet_return.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
 #IFACE=wlp2s0
-IFACE=enp1s0
+#IFACE=enp1s0
 ALARM_AUDIO_FILE=alarm.wav
-HOST_TO_PING=192.168.0.1
+HOST_TO_PING=8.8.8.8
+RETRY_TIME=7
 
 if [[ "$IFACE" = "" ]]; then
-	export IFACE=eth0
-	printf "IFACE was set to %s\n" ${IFACE}
+	printf "Using default route to ping.\n"
+	PING_CMD="ping -c 1 ${HOST_TO_PING} > /dev/null"
 else
 	printf "IFACE get from environ(%s)\n" ${IFACE}
+	PING_CMD="ping -I ${IFACE} -c 1 ${HOST_TO_PING} > /dev/null"
 fi
-
-
-PING_CMD="ping -I ${IFACE} -c 1 ${HOST_TO_PING} > /dev/null"
 
 function play_sound()
 {
@@ -22,12 +21,17 @@ function play_sound()
 
 }
 
+function print_date() {
+	local connection_rec_time=$(date +%H:%m)
+	printf "Connection recovery at: %s\n" ${connection_rec_time}
+}
+
 
 until eval $PING_CMD; do
-  printf "ConnectionError, retry...\n"
-  sleep 3
+  sleep ${RETRY_TIME}
 done
 
+print_date
 play_sound
 
 exit 0
